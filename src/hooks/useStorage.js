5f2c6be420f9f69b4d7146a022a3fb6252d319e5
olivercastroteran <olivercastroteran@gmail.com>
2020-08-10 10:43:23 -0400

    UploadForm component styled

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react';
+import { projectStorage } from '../firebase/config';
+
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    // References
+    const storageRef = projectStorage.ref(file.name);
+  }, [file]);
+};
