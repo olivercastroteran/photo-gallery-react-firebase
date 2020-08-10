b4a22972817acf937a672240bf8c3167f89afd97
olivercastroteran <olivercastroteran@gmail.com>
2020-08-10 10:54:25 -0400

    firebase storage hook created

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -9,5 +9,24 @@ const useStorage = (file) => {
   useEffect(() => {
     // References
     const storageRef = projectStorage.ref(file.name);
+
+    storageRef.put(file).on(
+      'state_changed',
+      (snap) => {
+        let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percentage);
+      },
+      (err) => {
+        setError(err);
+      },
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        setUrl(url);
+      }
+    );
   }, [file]);
+
+  return { progress, error, url };
 };
+
+export default useStorage;
