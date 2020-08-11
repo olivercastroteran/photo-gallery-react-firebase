0a517c741771df11adfdf63ae39b816382d67615
olivercastroteran <olivercastroteran@gmail.com>
2020-08-11 18:13:20 -0400

    firestore hook created & rendering images to the DOM from firestore database

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+import { projectFirestoreDB } from '../firebase/config';
+
+const useFirestore = (collection) => {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    const unsub = projectFirestoreDB
+      .collection(collection)
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snap) => {
+        let documents = [];
+        snap.forEach((doc) => {
+          documents.push({ ...doc.data(), id: doc.id });
+        });
+        setDocs(documents);
+      });
+    return () => unsub();
+  }, [collection]);
+
+  return { docs };
+};
+
+export default useFirestore;
